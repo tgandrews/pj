41d95aded293980410226dc89c50479fe9098da5
Tom Andrews <git@tomandrews.co.uk>
2022-05-24 20:45:47 +0100

    Remove the title from the list

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use clap::{arg, command, Command};
+use projectron::config::ProjectDefinition;
 
 use crate::projectron::{config, tmux};
 
 mod projectron;
 
-fn get_project(project_name: &str) -> config::Project {
+fn get_project_defintions() -> Vec<ProjectDefinition> {
     let home_config = config::get_home_config();
-    for project_ref in home_config.project {
+    home_config.project
+}
+
+fn get_project(project_name: &str) -> config::Project {
+    let projects = get_project_defintions();
+    for project_ref in projects {
         if project_ref.name == project_name {
             return config::load_project(&project_ref);
         }
@@ -25,10 +31,26 @@ fn start(project_name: &str) {
 fn end(project_name: &str) {
     let project = get_project(project_name);
     if tmux::is_project_running(&project.name) {
-        tmux::kill(&project)
+        tmux::kill(&project);
+        println!("Successfully killed: {}", project_name)
     } else {
         println!("{} is not running", project.name)
     }
+}
+
+fn list() {
+    let running_projects = get_project_defintions()
+        .iter()
+        .filter(|p| tmux::is_project_running(&p.name))
+        .map(|p| p.name.clone())
+        .collect::<Vec<String>>();
+
+    if running_projects.is_empty() {
+        println!("No projects are running");
+        return;
+    }
+
+    println!("{}", running_projects.join("\n"));
 }
 
 fn main() {
@@ -38,13 +60,8 @@ fn main() {
         .arg_required_else_help(true)
         .subcommand(
             Command::new("end")
+                .alias("stop")
                 .about("End an existing session")
-                .arg_required_else_help(true)
-                .arg(arg!([PROJECT])),
-        )
-        .subcommand(
-            Command::new("stop")
-                .about("Stop an existing session")
                 .arg_required_else_help(true)
                 .arg(arg!([PROJECT])),
         )
@@ -54,12 +71,17 @@ fn main() {
                 .arg_required_else_help(true)
                 .arg(arg!([PROJECT])),
         )
+        .subcommand(
+            Command::new("list")
+                .alias("ls")
+                .about("Get the running projects"),
+        )
         .get_matches();
 
     match matches.subcommand() {
         Some(("start", sub_matches)) => start(sub_matches.value_of("PROJECT").unwrap()),
         Some(("end", sub_matches)) => end(sub_matches.value_of("PROJECT").unwrap()),
-        Some(("stop", sub_matches)) => end(sub_matches.value_of("PROJECT").unwrap()),
+        Some(("list", _)) => list(),
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
 }
