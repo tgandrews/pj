614f40c4a1e4c7bbec0ebaca14789f0469741ca1
Tom Andrews <git@tomandrews.co.uk>
2022-05-25 19:35:43 +0100

    Kill all child processes when ending a project

--- src/projectron/tmux.rs
+++ src/projectron/tmux.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 
 use super::config::{Project, Window};
 
@@ -92,23 +92,21 @@ fn run_tmux(args: Vec<&str>) {
 }
 
 pub fn is_project_running(name: &String) -> bool {
-    let output = Command::new("tmux")
-        .arg("ls")
-        .output()
-        .expect("failed to execute tmux ls");
+    let output = run_tmux_with_output(vec!["ls"]);
 
-    let stdout = String::from_utf8(output.stdout).unwrap();
-    let stderr = String::from_utf8(output.stderr).unwrap();
     if output.status.success() {
         let expected_string = format!("{}: ", name);
-        return stdout.contains(&expected_string);
+        return output.stdout.contains(&expected_string);
     }
 
     // No tmux sessions running
-    if stderr.starts_with("no server running on") {
+    if output.stderr.starts_with("no server running on") {
         return false;
     }
-    if stderr.starts_with("error connecting to /private/tmp/tmux") {
+    if output
+        .stderr
+        .starts_with("error connecting to /private/tmp/tmux")
+    {
         return false;
     }
     panic!(
@@ -117,7 +115,7 @@ pub fn is_project_running(name: &String) -> bool {
             .status
             .code()
             .expect("Unable to unwrap status code after tmux ls error"),
-        stderr
+        output.stderr
     )
 }
 
@@ -132,6 +130,71 @@ pub fn attach(project: &Project) {
     assert!(exit_status.success());
 }
 
+struct TmuxResult {
+    status: ExitStatus,
+    stderr: String,
+    stdout: String,
+}
+
+fn run_tmux_with_output(args: Vec<&str>) -> TmuxResult {
+    let output = Command::new("tmux")
+        .args(&args)
+        .output()
+        .expect("failed to execute tmux");
+
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let stderr = String::from_utf8(output.stderr).unwrap();
+
+    let result = TmuxResult {
+        status: output.status,
+        stderr,
+        stdout,
+    };
+
+    return result;
+}
+
+struct RunningPane<'a> {
+    project: &'a str,
+    pane_id: &'a str,
+    // running_command: &'a str,
+}
+
+fn kill_running_panes_for_project(project: &Project) {
+    let all_running_panes_output = run_tmux_with_output(vec![
+        "list-panes",
+        "-a",
+        "-F",
+        "#{pane_id},#{pane_current_command},#{session_name}",
+    ]);
+    if !all_running_panes_output.status.success() {
+        panic!(
+            "Unable to list tmux panes:\n{}",
+            all_running_panes_output.stderr
+        );
+    }
+    let running_panes_for_project: Vec<RunningPane> = all_running_panes_output
+        .stdout
+        .split("\n")
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            let values: Vec<&str> = line.split(",").collect();
+            return RunningPane {
+                pane_id: values[0],
+                // running_command: values[1],
+                project: values[2],
+            };
+        })
+        .filter(|pane| pane.project == project.name)
+        .collect();
+
+    for pane in running_panes_for_project {
+        run_tmux(vec!["send-keys", "-t", pane.pane_id, "C-c"])
+    }
+}
+
 pub fn kill(project: &Project) {
+    kill_running_panes_for_project(project);
+
     run_tmux(vec!["kill-session", "-t", project.name.as_str()]);
 }
